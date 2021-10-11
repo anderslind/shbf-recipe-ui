81ad9d2db18b04655ac91c8fc7878af986fd45d4
Anders Lindsgard <anders.lindsgard@docusign.com>
2021-10-11 21:16:28 +0200

    Bumping material ui to v5, Step 1.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ResponsiveDrawer from './components/ResponsiveDrawer/ResponsiveDrawer';
-import {makeStyles, MuiThemeProvider} from '@material-ui/core/styles';
-import {Box, createMuiTheme} from '@material-ui/core';
+import {makeStyles,} from '@material-ui/core/styles';
+import {Box} from '@material-ui/core';
 import Routes from './components/Routes/Routes';
 import SearchFilter from './components/SearchFilter/SearchFilter/SearchFilter';
 import {useSetRecoilState} from 'recoil';
@@ -10,47 +10,6 @@ import {recipeFilter, EMPTY_STATE} from './state';
 
 export const drawerWidth = 375;
 export const captionColor = '#888';
-
-const customTheme = createMuiTheme({
-    typography: {
-        fontFamily: 'Roboto Condensed'
-    },
-    palette: {
-        primary: {
-            // light: will be calculated from palette.primary.main,
-            main: '#da3301',
-            // dark: will be calculated from palette.primary.main,
-            // contrastText: will be calculated to contrast with palette.primary.main
-        },
-        secondary: {
-            main: '#888'
-        },
-        background: {
-            default: '#fff',
-        }
-    },
-    breakpoints: {
-        // Define custom breakpoint values.
-        // These will apply to Material-UI components that use responsive
-        // breakpoints, such as `Grid` and `Hidden`. You can also use the
-        // theme breakpoint functions `up`, `down`, and `between` to create
-        // media queries for these breakpoints
-        values: {
-            xs: 0,
-            sm: 760,
-            md: 1020,
-            lg: 1280,
-            xl: 1920
-        }
-    },
-    theme: {
-        mixins: {
-            toolbar: {
-                minHeight: 58
-            }
-        }
-    }
-});
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -84,7 +43,7 @@ function App() {
     };
 
     return (
-        <MuiThemeProvider theme={customTheme} >
+        <>
             <CssBaseline />
             <Box className={classes.root} displayname={'App'}>
                 <ResponsiveDrawer mobileOpen={mobileOpen} onClearFilter={onClearFilter} handleDrawerToggle={handleDrawerToggle}>
@@ -92,7 +51,7 @@ function App() {
                 </ResponsiveDrawer>
                 <Routes handleDrawerToggle={handleDrawerToggle} />
             </Box>
-        </MuiThemeProvider>
+        </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,12 +7,56 @@ import {
     RecoilRoot
 } from 'recoil';
 import DebugObserver from "./debug/DebugObserver";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+
+const customTheme = createMuiTheme({
+    typography: {
+        fontFamily: 'Roboto Condensed'
+    },
+    palette: {
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: '#da3301',
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+        },
+        secondary: {
+            main: '#888'
+        },
+        background: {
+            default: '#fff',
+        }
+    },
+    breakpoints: {
+        // Define custom breakpoint values.
+        // These will apply to Material-UI components that use responsive
+        // breakpoints, such as `Grid` and `Hidden`. You can also use the
+        // theme breakpoint functions `up`, `down`, and `between` to create
+        // media queries for these breakpoints
+        values: {
+            xs: 0,
+            sm: 760,
+            md: 1020,
+            lg: 1280,
+            xl: 1920
+        }
+    },
+    theme: {
+        mixins: {
+            toolbar: {
+                minHeight: 58
+            }
+        }
+    }
+});
 
 ReactDOM.render(
   <React.StrictMode>
       <RecoilRoot>
         <DebugObserver />
-        <App />
+          <MuiThemeProvider theme={customTheme} >
+            <App />
+          </MuiThemeProvider>
       </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
